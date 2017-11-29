a291379161568691c35542cad23c96207b6b63c7
anaisrabary <anais.rabary@mail.com>
2017-11-29 10:44:47 +0100

    send UDP et receiveUDP OK (rédiger les tests de façon propre)

--- src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
@@ -1,8 +1,17 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class UDPMessageReceiverService implements MessageReceiverService {
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+        DatagramSocket receiverSocket = new DatagramSocket(port);
+        DatagramPacket receiverPacket = new DatagramPacket(new byte[1000],1000); // TEst
+        receiverSocket.receive(receiverPacket);
+        byte[] data = receiverPacket.getData();
+        String dataText= new String(data);
+        System.out.println(dataText);
+        // throw new UnsupportedOperationException();
     }
 }
